42110d0d05ef414785e0283bd8d15cee76effaa0
michaeljamie <familiarfletcher@gmail.com>
2018-07-26 16:09:41 -0600

    fixed auth0

--- server/server.js
+++ server/server.js
@@ -108,7 +108,7 @@ app.get('/auth/callback', async (req, res) => {
       client_secret: CLIENT_SECRET,
       code: req.query.code,
       grant_type: 'authorization_code',
-      redirect_uri: `http://${req.headers.host}/auth/callback`
+      redirect_uri: `${process.env.PROTOCOL}://${req.headers.host}/auth/callback`
     };
   
     // post request to exchange the code for a token
